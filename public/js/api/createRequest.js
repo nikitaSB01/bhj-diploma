9941659e8311fe58374ea97c35bc34632d6d075d
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-01-13 16:11:13 +0600

    createRequest прописал

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,10 +2,46 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {};
+const createRequest = (options = {}) => {
+  if (options) {
+    const xhr = new XMLHttpRequest();
+    let formData = new FormData();
+    let urlObj = options.url;
+    if (options.method !== "GET") {
+      Object.entries(options.data).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+    } else {
+      formData = "";
+      if (!urlObj.includes("/account")) {
+        urlObj += "?";
+        Object.entries(options.data).forEach(
+          ([key, value]) => (urlObj += `${key}=${value}&`)
+        );
+        //? // // // // // //
+        urlObj = urlObj.slice(0, -1);
+      }
+    }
+    try {
+      xhr.open(options.method, urlObj);
+      xhr.send(formData);
+    } catch (err) {
+      options.callback(err, null);
+    }
+    xhr.responseType = "json";
+    //? что именно => ...
+    xhr.load = () => {
+      options.callback(null, xhr.response);
+    };
+    /*   xhr.addEventListener("readystatechange", function () {
+      if (xhr.status === 200 && xhr.readyState === xhr.DONE) {
+        options.callback(null, xhr.response);
+      }
+    }); */
+  }
+};
 
-/* 
-
+/*
 const createRequest = (options = {}) => {
   const xhr = new XMLHttpRequest();
   xhr.responseType = "json";
