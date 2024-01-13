85f8022acd13ca9a61043ec0688c6fdc4f63bdaf
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-01-13 23:03:58 +0600

    добавил UserWid

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -11,8 +11,11 @@ class UserWidget {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor(element){
-
+  constructor(element) {
+    if (!element) {
+      throw new Error("Невалидное значение для UserWidget");
+    }
+    this.element = element;
   }
 
   /**
@@ -22,7 +25,10 @@ class UserWidget {
    * в элемент .user-name устанавливает имя
    * авторизованного пользователя
    * */
-  update(){
-
+  update() {
+    if (User.current()) {
+      const userName = document.querySelector(".user-name");
+      userName.textContent = User.current().name;
+    }
   }
 }
